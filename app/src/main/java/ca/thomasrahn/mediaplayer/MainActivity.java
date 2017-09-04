e769716eb7b53e468d128925a73e5cf0bbfd6ab8
unknown <thomas@rahn.ca>
2017-09-04 11:43:39 -0400

    Added activity for video. Started implementation on file browser

--- app/src/main/java/ca/thomasrahn/mediaplayer/MainActivity.java
+++ app/src/main/java/ca/thomasrahn/mediaplayer/MainActivity.java
@@ -1,7 +1,17 @@
 package ca.thomasrahn.mediaplayer;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import java.io.File;
+import java.io.FilenameFilter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button)findViewById(R.id.play_video);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    //In an Activity
+    private String[] mFileList;
+    private File mPath = new File(Environment.getExternalStorageDirectory() + "//yourdir//");
+    private String mChosenFile;
+    private static final String FTYPE = ".txt";
+    private static final int DIALOG_LOAD_FILE = 1000;
+
+    private void loadFileList() {
+        try {
+            mPath.mkdirs();
+        }
+        catch(SecurityException e) {
+
+        }
+        if(mPath.exists()) {
+            FilenameFilter filter = new FilenameFilter() {
+
+                @Override
+                public boolean accept(File dir, String filename) {
+                    File sel = new File(dir, filename);
+                    return filename.contains(FTYPE) || sel.isDirectory();
+                }
+
+            };
+            mFileList = mPath.list(filter);
+        }
+        else {
+            mFileList= new String[0];
+        }
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        switch(id) {
+            case DIALOG_LOAD_FILE:
+                builder.setTitle("Choose your file");
+                if(mFileList == null) {
+                    dialog = builder.create();
+                    return dialog;
+                }
+                builder.setItems(mFileList, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mChosenFile = mFileList[which];
+                        //you can do stuff with the file here too
+                    }
+                });
+                break;
+        }
+        dialog = builder.show();
+        return dialog;
     }
 }
